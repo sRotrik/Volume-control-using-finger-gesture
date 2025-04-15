8ed62bf088421beb30479676c8f0fd49969d230f
SROTRIK PRADHAN <171825173+sRotrik@users.noreply.github.com>
2025-04-15 09:22:21 +0530

    Volume-control-using-finger-gesture
    Controlling the volume bar using finger gesture

---
+++ advance.py
@@ -0,0 +1,96 @@
+import cv2
+import time
+import numpy as np
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+import HandTrackingModule as htm
+
+################################
+wCam, hCam = 800, 550
+################################
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
+
+detector = htm.handDetector(detectionCon=0.7)
+
+# Audio setup
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None
+)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+volRange = volume.GetVolumeRange()
+
+minVol = volRange[0]
+maxVol = volRange[1]
+vol = 0
+volBar = 400
+volPer = 0
+
+volLocked = False  # Lock flag
+
+while True:
+    success, img = cap.read()
+    if not success:
+        continue
+
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+
+    if lmList:
+        # Thumb and index
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        # Draw hand landmarks
+        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        # Check if pinky is down (tip is below its lower joint)
+        pinky_tip = lmList[20][2]
+        pinky_lower = lmList[18][2]
+        if pinky_tip > pinky_lower:
+            volLocked = True
+            cv2.putText(img, "Volume Locked", (400, 100), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+        else:
+            volLocked = False
+
+        # Volume Control if not locked
+        if not volLocked:
+            vol = np.interp(length, [50, 300], [minVol, maxVol])
+            volBar = np.interp(length, [50, 300], [400, 150])
+            volPer = np.interp(length, [50, 300], [0, 100])
+
+            volume.SetMasterVolumeLevel(vol, None)
+
+        # Visual cue when fingers are close
+        if length < 50:
+            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+
+    # Draw volume bar
+    cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
+    cv2.putText(img, f'{int(volPer)}%', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+
+    # FPS
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+
+    cv2.imshow("Img", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
